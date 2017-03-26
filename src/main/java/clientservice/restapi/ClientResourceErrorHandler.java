063c589584a002abb841005ad6dd12a47f37e379
dserradji <dserradji@outlook.com>
2017-03-26 17:54:45 -0400

    @ControllerAdvice replaced with @RestControllerAdvice

--- src/main/java/clientservice/restapi/ClientResourceErrorHandler.java
+++ src/main/java/clientservice/restapi/ClientResourceErrorHandler.java
@@ -7,22 +7,19 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ClientResourceErrorHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(ClientResourceException.class)
-	@ResponseBody
 	ResponseEntity<?> handleControllerException(ClientResourceException ex) {
 		return new ResponseEntity<>(ex.getMessage(), ex.getHttpStatus());
 	}
 
 	@ExceptionHandler(Exception.class)
-	@ResponseBody
 	ResponseEntity<?> handleControllerException(HttpServletRequest request, Exception ex) {
 		
 		final Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
