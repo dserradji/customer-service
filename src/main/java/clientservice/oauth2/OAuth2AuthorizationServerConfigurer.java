df2fefa652cabac77b85e98ede942604bdca398a
dserradji <dserradji@outlook.com>
2017-04-07 18:10:17 -0400

    OAuth2 config classes taken out of ClientService class

---
+++ src/main/java/clientservice/oauth2/OAuth2AuthorizationServerConfigurer.java
@@ -0,0 +1,30 @@
+package clientservice.oauth2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@EnableAuthorizationServer
+@Configuration
+public class OAuth2AuthorizationServerConfigurer extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager);
+	}
+
+	/* OAuth2 in memory credentials */
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("clientId").secret("clientSecret").scopes("read", "read-all", "write").and()
+				.build();
+	}
+
+}
